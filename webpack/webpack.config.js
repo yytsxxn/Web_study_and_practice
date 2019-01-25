de5a48a7a0d5586b86d8cc9c44819e52840d2ee7
yytsxxn <1753337170@qq.com>
2019-01-25 11:39:06 +0800

    webpack

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,10 +1,12 @@
 const path = require('path');
-module.export = {
+module.exports = {
+    mode:'development',
+    // 开发环境  production(生产环境) 
     entry:{
         xx:'./src/index.js'
     },
     output:{
-        path:path.resolve(__dirname,'/dist'),
-        filename:'[name].js'
+        path:path.resolve(__dirname,'dist'),
+        filename:'[name].js'  
     }
 }
