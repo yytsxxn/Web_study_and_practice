3bf8e146ff055df884dcfdd8007fa781ae30ef22
yytsxxn <1753337170@qq.com>
2019-02-13 16:39:42 +0800

    vueapp

---
+++ vueproject/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <c-header></c-header>
+  </div>
+</template>
+
+<script>
+  import CHeader from '@/components/CHeader';
+  export default {
+    components :{
+      CHeader
+    }
+  }
+</script>
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+</style>
---
+++ vueproject/src/components/CHeader.vue
@@ -0,0 +1,15 @@
+<template>
+    <div id="header">
+        首页
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+
+</style>
