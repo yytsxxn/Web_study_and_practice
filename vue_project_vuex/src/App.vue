67bb3bacaabc6313dc8bedd447d3ce502b22bc4a
yytsxxn <1753337170@qq.com>
2019-02-15 13:49:31 +0800

    vuex

---
+++ vue_project_vuex/src/App.vue
@@ -0,0 +1,59 @@
+<template>
+  <div id="app">
+      <c-header></c-header>
+      <div id="content">
+          <router-view></router-view>
+      </div>
+      <c-footer :menu="menu"></c-footer>
+  </div>
+</template>
+<script>
+import CHeader from "@/components/CHeader.vue";
+import CFooter from "@/components/CFooter.vue";
+import {mapMutations} from "vuex";
+export default {
+  data(){
+    return {
+       menu:[
+         {
+           path:"/movie",
+           name:"电影",
+           bgColor:"#ff0036"
+         },
+         {
+           path:"/music",
+           name:"音乐",
+           bgColor:"#e9f460"
+         },{
+           path:"/book",
+           name:"书籍",
+           bgColor:"#96c2f1"
+         },{
+           path:"/photo",
+           name:"图片",
+           bgColor:"#4abce8"
+         }
+       ]
+    }
+  },
+  components: {
+    CHeader,
+    CFooter
+  },
+  methods:mapMutations(["change"]),
+  created () {
+      let result = this.menu.filter((obj,index)=>{
+        return obj.path == this.$route.path;
+      });
+      if(result.length){
+        // 修改state中存的name color 改成result[0].name result[0].bgColor
+        this.change(result[0]);
+      }
+  }
+};
+</script>
+<style>
+#content {
+  margin: 1rem 0;
+}
+</style>
---
+++ vue_project_vuex/src/components/CFooter.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="footer" :style="{background:color}" >
+        <ul>
+            <li v-for="(obj,index) in menu" :key="index" >
+                <router-link :to="obj.path" @click.native="change(obj)" >{{obj.name}}</router-link>
+            </li>
+        </ul>
+
+    </div>
+</template>
+
+<script>
+    import {mapState,mapMutations} from "vuex";
+    export default {
+        props:["menu"],
+        computed: mapState(["color","name"]),
+        methods: mapMutations(["change"])
+    }
+</script>
+
+<style scoped>
+    .footer{
+        position: fixed;
+        bottom: 0;
+        left:0;
+        height: 1rem;
+        background: #ff0036;
+        width:100%;
+    }
+    .footer ul{
+        display: flex;
+    }
+    li{
+        flex-grow:1;
+        color: #fff;
+        line-height:  1rem; 
+        text-align: center;
+    }
+    .footer a{
+        color: #000;
+        outline:none;
+    }
+    .footer a.router-link-active{
+        color: #fff;
+    }
+</style>
